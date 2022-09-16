de71294f1e31d1c51cf1fe9e99a026d5a71e2c51
AlikMatveev <alik.matveev.16@gmail.com>
2022-09-16 18:43:21 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,127 +1,110 @@
-const startBtn = document.querySelector('.start-btn')
-const block = document.querySelector('.block');
-let current = 0;
-const results = []
+const copyYear = document.querySelector(".year");
+const login = document.querySelector(".login-btn");
+const lawer = document.querySelector("#lawer");
+const contacts = document.querySelector("#contacts")
 
-let data = []
+copyYear.innerHTML = new Date().getFullYear();
 
-fetch("getData.php").then(res => res.json()).then(json => {
-  const keys = json.values.slice(0,1);
-  data = json.values.slice(1).reduce((acc, el) => {
-  let obj = el.reduce((arr, value, i) => {
-      const k = keys[0][i];
-      if(k.match(/^answer__\d/)){
-        if(value !== ""){
-          arr.answers.push(value);
-        }
-      }else if(k.match(/^score__\d/)){
-          arr.scores.push(value);
-      }else arr[k] = value;
-      return arr;
-    }, {answers: [], scores: []});
-    acc.push(obj)
-    return acc;
-  }, []);
-});
+const modal = new Modal({maxWidth: "400px"});
+const modal2 = new Modal({maxWidth: "600px"})
 
-
-const createContent = (data, current) => {
-  const {question, answers, scores} = data[current];
-  return `
-  <div class="quiz">
-    <div class="quiz-header">
-      <p>${current + 1}/${data.length}</p>
-    </div>
-    <div class="quiz-container">
-      <div class="quiz-question">
-        <p class="quiz-question__text">${question}</p>
-        <form class="quiz-question__answers">
-        	${answers.map((item, index) => `<div class="quiz-question"><label><input value="${item}" type="radio" data-score="${scores[index]}" name="answer">${item}</label></div>`).join('')}
-  				<button class="btn quiz-submit">Подтвердить</button>
+login.addEventListener('click', () => {
+    modal.open();
+    modal.setContent(`
+        <h2 class="modal__title">Авторизация</h2>
+        <form class="login-modal__form">
+            <input type="text" placeholder="Логин:" name="login" id="login" class="login-form__input" required>
+            <input type="password" placeholder="Пароль:" name="password" id="password" class="login-form__input" required>
+            <div class="form__group">
+                <button class="btn login-form__btnlogin" type="button">Войти</button>
+                <button class="btn login-form__change" type="button">Забыли пароль</button>
+            </div>
         </form>
-      </div>
+    `);
+  });
+
+lawer.addEventListener("click", () => {
+    modal2.open();
+    modal2.setContent(`
+    <div class="lawer-container">
+        <h2 class="modal__title">Юридическая информация</h2>
+
+        <div class="lawer-info__group">
+            <p>Название:</p>
+            <p>ООО «ЛР»</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>ИНН:</p>
+            <p>7728767260</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>КПП:</p>
+            <p>772801001</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>ОГРН:</p>
+            <p>1117746212210</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>Юридический адрес:</p>
+            <p>Российская Федерация, 117574, г. Москва, проезд Одоевского, д.3, корп.7, ЭТ/ПОМ/ОФ 1/II/14</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>Фактический адрес:</p>
+            <p>Российская Федерация, 115419, г. Москва, 2-й Верхний Михайловский проезд, д. 9, стр. 2, офис 501</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>Реквизиты:</p>
+            <p>АКБ «Абсолют Банк» (ПАО) г. Москва</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>Расчетный счет:</p>
+            <p>40702810422000029070</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>К/С:</p>
+            <p>30101810500000000976</p>
+        </div>
+
+        <div class="lawer-info__group">
+            <p>БИК:</p>
+            <p>044525976</p>
+        </div>
     </div>
-  </div>`
-}
+    `)
+})
 
-const createCoupon = (text, color) => {
-  const [h2, p] = text;
-  return `
-  <div class="coupon ${color}">
-    <div class="coupon__info">
-      <h2>${h2}</h2>
-      <p>${p}</p>
+
+contacts.addEventListener('click', () => {
+    modal2.open();
+    modal2.setContent(`
+    <div class="contacts-container">
+        <h2 class="modal__title">Контакты</h2>
+
+        <div class="contacts-info__group">
+            <img src="img/icon-phone.png">
+            <a class="bold" href="tel:74996536610">+7 (499) 653 - 66 - 10</a>
+        </div>
+
+        <div class="contacts-info__group">
+            <img src="img/icon-mail.png">
+            <a class="bold" href="mailto:info@ligand-research.com">info@ligand-research.com</a>
+        </div>
+
+        <div class="contacts-info__group">
+            <img src="img/icon-geo.png">
+            <a href="mailto:info@ligand-research.com">115419, г. Москва, 2-й Верхний Михайловский проезд, д. 9, стр. 2, офис 501</a>
+        </div>
+
+        <iframe style="margin-top: 20px "src="https://yandex.ru/map-widget/v1/?lang=ru_RU&amp;scroll=true&amp;um=constructor%3Ab5f2a5270066ba9ecabc78a68b0d72e49307644948dce71771507701f6029840" frameborder="0" allowfullscreen="true" width="100%" height="400px" style="display: block;"></iframe>
     </div>
-    <form class="form-data">
-      <input type="text" placeholder="Введите ваш номер телефона:" name="phone" required />
-      <button class="btn formdata-btn" type="submit">Подтвердить</button>
-    </form>
-  </div>`;
-}
-
-const result = (arr) => {
-  const scores = arr.map(el => +el.score).reduce((a,b) => a+b);
-  const allScores = Math.floor(data.map(({answers}) => answers.length).reduce((a,b) => a+b));
-
-  if(scores <= Math.floor(allScores/3)){
-    block.innerHTML = createCoupon(["Ваша коробка передач в хорошем состоянии.", "Не забывайте менять масло чтобы она и дальше работала хорошо. Оставьте свой номер телефона и мы отправим вам персональный купон на скидку в 2000р. на замену масла."], "green")
-  }else if(scores > Math.floor(allScores/3) && scores <= Math.floor(2*(allScores/3))){
-    block.innerHTML = createCoupon(["Ваша коробка передач работает не очень хорошо.", "Диагностика поможет выявить неисправности. Пройдите консультацию по телефону у мастера и запишитесь на бесплатную диагностику в нашем сервисе."], "yellow");
-  }else{
-    block.innerHTML = createCoupon(["Ваша коробка передач неисправна.", "Срочно пройдите бесплатную диагностику в нашем сервисе и выявите неисправности. В случае ремонта ваша персональная скидка составит 10% от стоимости."], "red");
-  }
-}
-
-
-const generateBlock = (index) => block.innerHTML = createContent(data, index)
-
-block.addEventListener('click', e => {
-  if(e.target.matches('.quiz-submit')){
-    e.preventDefault();
-    const form = e.currentTarget.querySelector('form');
-    const answer = form.answer.value;
-    if(!answer){
-      alert('Не выбран вариант');
-    }else{
-      const score = [...form.answer].filter(el => el.value == form.answer.value)[0].dataset.score;
-      results[current] = {question: data[current].question, answer, score};
-      if(current >= data.length - 1) return result(results);
-      generateBlock(current += 1);
-    }
-  }
-  if(e.target.matches(".formdata-btn")){
-    e.preventDefault();
-    const form = e.currentTarget.querySelector('form');
-    if(!form.phone.value.match(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/gi)){
-      alert("Введите корректый номер телефона");
-      return null;
-    }
-    const formData = new FormData(form);
-    formData.append("results", JSON.stringify(results));
-    fetch("main.php", {
-        method: "POST",
-        body: formData
-      }).then(data => block.innerHTML = `
-        <div class="thanks">
-          <p>Благодарим вас за заполнение заявки!</p>
-          <a href="https://t-first.ru/">Автосервис T-FIRST</a>
-        </div>`);
-  }
-});
-
-startBtn.addEventListener("click", () => generateBlock(current));
-
-const anchors = document.querySelectorAll('a[href*="#start"]')
-console.log(anchors);
-anchors.forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-
-    const blockID = anchor.getAttribute('href');
-    console.log(blockID);
-    document.querySelector(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-});
+    `)
+})
